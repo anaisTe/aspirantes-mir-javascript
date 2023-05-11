be0762bcf2e6a6dd43afb7e77b7dfbd4a4f33d0a
anaisTe <angieanais@gmail.com>
2023-05-10 19:45:47 -0500

    solucion a los ejercicios de js

---
+++ js-1/IMC.js
@@ -0,0 +1,6 @@
+function bmi(peso, altura) {
+    let formula = peso / (altura * altura);
+    return formula
+}
+
+console.log(bmi(72, 1.6))
---
+++ js-1/conditionals.js
@@ -0,0 +1,16 @@
+function calcularColor(num) {
+    var color;
+    if (num === 1) {
+      color = "negro";
+    } else if (num === 2) {
+      color = "blanco";
+    } else if (num === 3) {
+      color = "azul";
+    } else {
+      color = "verde";
+    }
+  
+    return `El color es ${color}`;
+  }
+
+  console.log(calcularColor(1))
